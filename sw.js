1d4a479baff3020f94d22614237a3ebd59305d5f
Christopher Lopes <christopher.lopes@gmail.com>
2018-10-13 19:26:40 -0700

    Trying network then falling back to cache service worker

--- sw.js
+++ sw.js
@@ -1,28 +1,52 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('v1').then(function(cache) {
-      return cache.addAll([
-        '/soma-check-in/',
-        '/soma-check-in/index.html',
-        '/soma-check-in/images/icons/icon-128x128.png',
-        '/soma-check-in/images/icons/icon-144x144.png',
-        '/soma-check-in/images/icons/icon-152x152.png',
-        '/soma-check-in/images/icons/icon-192x192.png',
-        '/soma-check-in/images/icons/icon-384x384.png',
-        '/soma-check-in/images/icons/icon-512x512.png',
-        '/soma-check-in/images/icons/icon-72x72.png',
-        '/soma-check-in/images/icons/icon-96x96.png'
-      ]);
-    })
-  );
+var CACHE = 'network-or-cache';
+
+self.addEventListener('install', function(evt) {
+  console.log('The service worker is being installed.');
+
+  evt.waitUntil(precache());
 });
 
-self.addEventListener('fetch', function(event) {
- console.log(event.request.url);
+self.addEventListener('fetch', function(evt) {
+  console.log('The service worker is serving the asset.');
 
- event.respondWith(
-   caches.match(event.request).then(function(response) {
-     return response || fetch(event.request);
-   })
- );
+  evt.respondWith(fromNetwork(evt.request, 400).catch(function () {
+    return fromCache(evt.request);
+  }));
 });
+
+function precache() {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.addAll([
+      '/soma-check-in/',
+      '/soma-check-in/index.html',
+      '/soma-check-in/images/icons/icon-128x128.png',
+      '/soma-check-in/images/icons/icon-144x144.png',
+      '/soma-check-in/images/icons/icon-152x152.png',
+      '/soma-check-in/images/icons/icon-192x192.png',
+      '/soma-check-in/images/icons/icon-384x384.png',
+      '/soma-check-in/images/icons/icon-512x512.png',
+      '/soma-check-in/images/icons/icon-72x72.png',
+      '/soma-check-in/images/icons/icon-96x96.png'
+    ]);
+  });
+}
+
+function fromNetwork(request, timeout) {
+  return new Promise(function (fulfill, reject) {
+    var timeoutId = setTimeout(reject, timeout);
+
+    fetch(request).then(function (response) {
+      clearTimeout(timeoutId);
+      fulfill(response);
+    }, reject);
+  });
+}
+
+function fromCache(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
+
